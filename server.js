60f5a4e2f9e302bddbafcf66490a8b96537acfd7
Tcomp <williamt7786@gmail.com>
2021-06-11 19:44:10 -0400

    created new route files and continued adding to server

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const express = require ("express");
 const app = express();
 const Port = process.env || 8080;
 
+app.use(express.urlencoded({ extended:true}));
+app.use(express.json());
+
+
+
+require("./route/api.js")(app);
+require("./route/html")(app);
