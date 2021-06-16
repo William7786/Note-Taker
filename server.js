4c64b50e8cd526888c7e0972a2f547e545d9befb
Tcomp <williamt7786@gmail.com>
2021-06-15 21:53:49 -0400

    for error fix

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require ("express");
 const app = express();
-const PORT = process.env || 8080;
+const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended:true}));
+app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
-app.use(express.static("./public/"))
+app.use(express.static(__dirname + "./public"))
 
 
 require("./route/api.js")(app);
 require("./route/html.js")(app);
 
 app.listen(PORT, function(){
-    console.log("listening on " + PORT)
+    console.log(`listening on : http://localhost:${PORT}`)
 })
