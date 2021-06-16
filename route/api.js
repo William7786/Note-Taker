8c30e0c13a56b50bc363e709965d8a4ebdb57a62
Tcomp <williamt7786@gmail.com>
2021-06-16 17:24:49 -0400

    fixing styling issues

--- route/api.js
+++ route/api.js
@@ -1,11 +1,25 @@
+const fs = require("fs");
 const data = require ("../db/db.json");
+const {v4: uuidv4} = require("uuid")
 module.exports = function (app){
     app.get("/api/notes/", function(req,res){
-        res.json(note_data);
+        res.json(data);
+    })
+
+    app.get("/api/notes/:id", (req, res) => {
+        res.json(data[Number(req.params.id)])
     })
     app.post("/api/notes", function(req,res){
-note_data.push(req.body);
-res.json(true);
+        let note = req.body;
+        let ID = data.length;
+        note.id = ID;
+        req.body.id = uuidv4();
+
+        fs.writeFileSync("/db/db.json", JSON.stringify(data), (error)=>{
+            if (error) console.log(err)
+        })
+// note_data.push(req.body);
+// res.json(true);
     })
 
 }
