5d54318a31802dcca6fa6669bb8c152e865affdf
Tcomp <williamt7786@gmail.com>
2021-06-15 22:30:54 -0400

    fixed dir name error

--- route/html.js
+++ route/html.js
@@ -2,9 +2,9 @@ const path = require("path")
 
 module.exports = (app)=> {
     app.get("/notes",(req, res) => {
-        res.sendfile(path.join(_dirname, "/../public/notes.html"));
+        res.sendfile(path.join(__dirname, "/../public/notes.html"));
     })
     app.get("/", function(req,res){
-        res.sendFile(path.join(_dirname, "/../public/index.html"));
+        res.sendFile(path.join(__dirname, "/../public/index.html"));
     })
 }
